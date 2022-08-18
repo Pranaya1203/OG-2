52839c98f03491c1635b9e83fe1d386d113cfd0d
Pranaya1203 <111361512+Pranaya1203@users.noreply.github.com>
2022-08-18 13:04:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var penguin, penguin_dancing, penguinImg;
+var obstacle, obstacle1, obstacle2, obstacle3;
+var obstaclesGroup;
+var ground, groundImg;
+var backgroundImg;
+
+function preload(){
+    backgroundImg = loadImage("Antarctica.jpg");
+    penguinImg = loadImage("")
+
+
+
+
+}
